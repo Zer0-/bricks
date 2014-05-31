240af2e7caaeee7819e2383f7a63f083269147a3
Philipp Volugine <phil.volguine@gmail.com>
2014-05-31 13:56:33 -0400

    Refactoring router - work in progress
    route function that attaches api to request
    will be put into the api and we will have a
    request subclass that adds this api during routing.
    
    routing also doesn't try to calculate everything
    in one pass anymore.

--- pyramid_bricks/routing.py
+++ pyramid_bricks/routing.py
@@ -1,5 +1,49 @@
 from ceramic_forms import Form
 
+def _match_pathpart(routemap, part):
+    #wrap schema and pathpart in a list because Form cannot handle naked values.
+    subject = [part]
+    for key in routemap:
+        validator = Form([key])
+        if validator.validate(subject):
+            return validator.cleaned[0], routemap[key]
+
+def _match_routes(path, routemap):
+    matched = [('/', routemap)]
+    for part in path:
+        match = _match_pathpart(routemap, part)
+        if match is None:
+            if routemap.handles_subtree == False:
+                return 404
+        else:
+            part, routemap = match
+        matched.append((part, routemap))
+    return matched
+
+class RequestRoute:
+    def __init__(self, request, routemap):
+        self.routemap = routemap
+        path = [p for p in request.path.split('/') if len(p)]
+        self._matched_routes = _match_routes(path, routemap)
+
+    @property
+    def path(self):
+        return [p for p, _ in self._matched_routes[1:]]
+
+    @property
+    def vars(self):
+        routemap = self.routemap
+        vars = []
+        for part, route in self._matched_routes:
+            if part in routemap:
+                routemap = route
+            elif route != routemap:
+                vars.append(part)
+                routemap = route
+        return vars
+
+#OLD:
+
 class RequestRouteApi:
     def __init__(self, vars):
         self.vars = vars
--- pyramid_bricks/test/test_router.py
+++ pyramid_bricks/test/test_router.py
@@ -3,7 +3,12 @@ from pyramid_bricks.routing import routing
 from webob import Request
 
 class Route(dict):
-    def __init__(self, handles_subtree=False):
+    def __init__(
+        self,
+        permissions=(),
+        handles_subtree=False
+    ):
+        self.permissions = permissions
         self.handles_subtree = handles_subtree
 
     def __add__(self, subtree):
@@ -106,6 +111,53 @@ class TestRequestRouteApi(unittest.TestCase):
         route = routing(url_tree, request)
         vars = request.route.vars
         self.assertEqual(vars, [1234, 'something'])
+
+#New code (refactor above)
+from ceramic_forms import Use
+from pyramid_bricks.routing import RequestRoute
+
+class TestRequestRoute(unittest.TestCase):
+    def setUp(self):
+        self.root = Route()
+        self.r1 = Route()
+        self.r2 = Route(handles_subtree=True)
+        self.r3 = Route()
+        self.r4 = Route(handles_subtree=True)
+        self.routemap = self.root + {
+            'first': self.r1 + {
+                Use(int): self.r2 + {
+                    'second': self.r3 + {
+                        Use(int): self.r4
+                    }
+                }
+            }
+        }
+
+    def testRouteMatching(self):
+        request = Request.blank('/first/1/one/two/second/2/three/four')
+        routeapi = RequestRoute(request, self.routemap)
+        shouldmatch = [
+            ('/', self.root),
+            ('first', self.r1),
+            (1, self.r2),
+            ('one', self.r2),
+            ('two', self.r2),
+            ('second', self.r3),
+            (2, self.r4),
+            ('three', self.r4),
+            ('four', self.r4)
+        ]
+        self.assertEqual(routeapi._matched_routes, shouldmatch)
+
+    def testTransformedPath(self):
+        request = Request.blank('/first/1/one/two/second/2/three/four')
+        routeapi = RequestRoute(request, self.routemap)
+        self.assertEqual(routeapi.path, ['first', 1, 'one', 'two', 'second', 2, 'three', 'four'])
+
+    def testVars(self):
+        request = Request.blank('/first/1/one/two/second/2/three/four')
+        routeapi = RequestRoute(request, self.routemap)
+        self.assertEqual(routeapi.vars, [1, 2])
 
 if __name__ == '__main__':
     unittest.main()
