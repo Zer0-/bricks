9e47f2f2a73274b4f822381c7e8e8d592ff106f2
Philipp Volugine <phil.volguine@gmail.com>
2014-06-06 15:14:00 -0400

    added json renderer

--- pyramid_bricks/render.py
+++ pyramid_bricks/render.py
@@ -13,6 +13,13 @@ def string_response(viewfn):
         return _response(view_result, 'text/plain')
     return wrapper
 
+def json_response(viewfn):
+    from json import dumps
+    def wrapper(clsinst, request):
+        view_result = viewfn(clsinst, request)
+        return _response(dumps(view_result), 'application/json')
+    return wrapper
+
 def mako_response(template_assetspec):
     template_filepath = AssetResolver().resolve(template_assetspec).abspath()
     template = Template(filename=template_filepath)
