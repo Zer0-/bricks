2744f540894fa28e0a03079c7b530afc74ac34c7
Philipp Volugine <phil.volguine@gmail.com>
2014-06-10 15:23:58 -0400

    fixed renderers to accept response objects

--- pyramid_bricks/render.py
+++ pyramid_bricks/render.py
@@ -1,33 +1,31 @@
-from webob import Response
 from mako.template import Template
 from pyramid.path import AssetResolver
 
-def _response(body, mime):
-    response = Response(content_type=mime)
-    response.text = body
-    return response
-
 def string_response(viewfn):
-    def wrapper(clsinst, request):
-        view_result = viewfn(clsinst, request)
-        return _response(view_result, 'text/plain')
+    def wrapper(clsinst, request, response):
+        view_result = viewfn(clsinst, request, response)
+        response.content_type =  'text/plain'
+        response.text = view_result
     return wrapper
 
 def json_response(viewfn):
     from json import dumps
-    def wrapper(clsinst, request):
-        view_result = viewfn(clsinst, request)
-        return _response(dumps(view_result), 'application/json')
+    def wrapper(clsinst, request, response):
+        view_result = viewfn(clsinst, request, response)
+        response.content_type = 'application/json'
+        response.text = view_result
     return wrapper
 
 def mako_response(template_assetspec):
     template_filepath = AssetResolver().resolve(template_assetspec).abspath()
     template = Template(filename=template_filepath)
     def render_decorator(viewfn):
-        def wrapper(clsinst, request):
-            view_result = viewfn(clsinst, request)
+        def wrapper(clsinst, request, response):
+            view_result = viewfn(clsinst, request, response)
             context = {'component': clsinst, 'request': request}
             context.update(view_result)
-            return _response(template.render(**context), 'text/html')
+            body = template.render(**context)
+            response.body = body
+            response.content_type = 'text/html'
         return wrapper
     return render_decorator
